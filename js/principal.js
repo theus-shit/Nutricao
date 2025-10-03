f92091e8c2dd3328374f3e99d8bd2736a83214eb
theus-shit <dias.matheus16@escola.pr.gov.br>
2025-10-03 11:12:30 -0300

    a

---
+++ js/principal.js
@@ -0,0 +1,10 @@
+var titulo = document.querySelector("h1");
+console.log(titulo);
+console.log(titulo.TextConsept);
+titulo.TextConsept = ("Aparecida Nutrição");
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = document.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+console.log(paciente);
+console.log(peso);
