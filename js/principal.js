87174f0bedac9baae06f1e266c461952b3e58611
theus-shit <dias.matheus16@escola.pr.gov.br>
2025-10-07 10:04:41 -0300

    45

--- js/principal.js
+++ js/principal.js
@@ -12,11 +12,11 @@ var tdPeso = document.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 console.log(peso);
 
-// 07/ 10/2025
 var tdAltura = document.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 console.log(altura);
 
+var tdImc = paciente.querySelector(".info-imc");
 var imc = peso / (altura * altura);
 tdImc.textContent = imc;
 console.log(imc);
