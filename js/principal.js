22fcbf722437c619f5eefb8f90366291fbf06490
theus-shit <dias.matheus16@escola.pr.gov.br>
2025-10-10 10:35:34 -0300

    5

--- js/principal.js
+++ js/principal.js
@@ -37,7 +37,7 @@ if (peso <0||peso> 1000) {
 if (altura <0||altura> 3.00){
     console.log("Altura inaválida!")
     alturaEhValido = false;
-    tdlmc.textContent="Altura inválida!";
+    tdlmc.textContent=("Altura inválida!");
 }
 
 if(alturaEhValida && pesoEhValido){
