33a21b0d3ca36615245911eb69f4ec772185b0a9
theus-shit <dias.matheus16@escola.pr.gov.br>
2025-11-04 09:43:19 -0300

    2

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -9,7 +9,7 @@ for (var i=0; i <= pacientes.length; i++){
 
 var paciente = pacientes[i];
 
-var tdPeso = paciente.querySelector(".info-peso")
+var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 
 var tdAltura = paciente.querySelector(".info-altura");
