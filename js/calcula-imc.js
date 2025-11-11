8b452c70881aeb8d7a4f470ada11e2ef7ec40f98
theus-shit <dias.matheus16@escola.pr.gov.br>
2025-11-11 09:14:54 -0300

    dc

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,51 +1,43 @@
 var titulo = document.querySelector("h1");
-console.log(titulo);
-console.log(titulo.textContent);
-titulo.textContent=("pinpin nutricao");
+titulo.textContent = ("Vlá Nutrição");
 
 var pacientes = document.querySelectorAll(".paciente");
+for (var i = 0; i < pacientes.length; i++) { // abre chaves do for 
+    var paciente = pacientes[i];
 
-for (var i=0; i <= pacientes.length; i++){
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-var paciente = pacientes[i];
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+    var tdImc = paciente.querySelector(".info-imc");
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var pesoEhValido = true;
+    var alturaEhValida = true;
 
-var tdImc = paciente.querySelector(".info-imc");
+    if (peso <= 0 || peso >= 1000) {
+        pesoEhValido = false;
+        tdImc.textContent = "Peso Inválido";
+        paciente.classList.add("paciente-invalido");
+    }
 
-// 10/10
+    if (altura <= 0 || altura >= 3.00) {
+        alturaEhValida = false;
+        tdImc.textContent = "Altura Inválida";
+        paciente.classList.add("paciente-invalido");
+    }
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+  if (alturaEhValida && pesoEhValido) {
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
+    }
+} 
 
-
-if (peso <=0||peso>=1000) {
-    console.log("Peso inválido!");
-    pesoEhValido = false;
-    tdImc.textContent="Peso inválido!";
-    paciente.classList.add("paciente-invalido")
-}
-
-if (altura <=0||altura>=3.00){
-    console.log("Altura inválida!");
-    alturaEhValida = false;
-    tdImc.textContent= "Altura inválida!";
-    paciente.classList.add("paciente-invalido");
-}
-
-if(alturaEhValida && pesoEhValido){
-    var imc = calculaImc (peso, altura);
-    tdImc.textContent = imc.toFixed(2);
-}
-}
-
-function calculaImc (peso, altura) {
+function calculaImc (peso, altura){
     var imc = 0;
     var imc = peso / (altura * altura);
     return imc.toFixed(2);
 }
+
 
